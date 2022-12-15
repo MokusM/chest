d2e03e4e0cefbf764fda2f5bfacfcc1d6dbf4903
Mykola Moiseiev <m.moceev@inchkiev.ua>
2022-12-15 23:26:49 +0200

    final

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="app">
-        <Game/>
+        <Game />
+        <div v-if="load" class="preloader">
+            <div class="preloader__content">
+                <div class="preloader__img">
+                    <img src="@img/preloader.gif" alt="">
+                </div>
+                <div class="preloader__text">Loading</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +20,45 @@ export default {
     components: {
         Game,
     },
+    data() {
+        return {
+            load: true
+        }
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.load = false
+        },1000)
+    }
 };
 </script>
+<style lang="scss" scoped>
+.preloader {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #000;
+    z-index: 300;
+
+    &__content {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
+    }
+
+    &__img {
+        width: 250px;
+        height: 250px;
+    }
+
+    &__text {
+        text-align: center;
+        color: #fff;
+        font-size: 50px;
+        margin-top: 15px;
+    }
+}
+</style>
